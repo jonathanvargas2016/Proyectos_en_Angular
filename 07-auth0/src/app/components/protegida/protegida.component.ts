fe67891dc364ab552ebc1b4c55cb72118befcf50
Jonathan S Vargas <jonathan.vargas01@epn.edu.ec>
2021-04-13 22:33:57 -0500

    uso de autenticacion auth0

---
+++ 07-auth0/src/app/components/protegida/protegida.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-protegida',
+  templateUrl: './protegida.component.html'
+})
+export class ProtegidaComponent implements OnInit {
+
+  constructor(public auth: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+}
