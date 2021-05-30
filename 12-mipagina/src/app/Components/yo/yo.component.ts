f239cd860a97f64b98263ba853c60515437d899a
Jonathan S Vargas <jonathan.vargas01@epn.edu.ec>
2021-05-29 23:03:58 -0500

    cambios en los iconos de informacion de contacto

--- 12-mipagina/src/app/Components/yo/yo.component.ts
+++ 12-mipagina/src/app/Components/yo/yo.component.ts
@@ -19,7 +19,12 @@ export class YoComponent implements OnInit {
     perfilProfesional: 'Soy egresado de la Facultad de Sistemas de la Escuela Politécnica Nacional, ' +
       'me considero una persona responsable, dinámica y creativa, con facilidad de adaptación y ' +
       'capacidad para trabajar en equipo, en condiciones de alta presión, con iniciativa para resolver ' +
-      'problemas eficientemente y lograr los objetivos y metas propuestos por la empresa. '
+      'problemas eficientemente y lograr los objetivos y metas propuestos por la empresa.',
+    motivacion: 'En la búsqueda de nuevas oportunidades laborales que me permitan crecer como profesional y a la vez como persona.' +
+      '          Tengo poco experiencia laboral por eso estoy dispuesto a trabajar con entusiamo en cualquier empresa ya sea pública o privada para' +
+      '          adquirir mas experiencia laboral, mejorar mis conocimientos y aprender acerca del modelo de negocio de esas empresas. Constantemente estoy aprendiendo' +
+      '          nuevas tecnologías para el desarrollo web en el Frontend y el Backend en las que utilizo frameworks, lenguajes de programación y base de datos.' +
+      '      '
   }
   informacionContacto = {
     correoPersonal: 'jonathan.1996mds@gmail.com',
