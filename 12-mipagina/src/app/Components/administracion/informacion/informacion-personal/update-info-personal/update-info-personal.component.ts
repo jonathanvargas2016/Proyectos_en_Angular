8cf7bb3e4aef8de138b10f46effdd1ff7dec5663
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2022-03-20 19:29:04 -0500

    fix

--- 12-mipagina/src/app/Components/administracion/informacion/informacion-personal/update-info-personal/update-info-personal.component.ts
+++ 12-mipagina/src/app/Components/administracion/informacion/informacion-personal/update-info-personal/update-info-personal.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {InfoPersonalService} from "../../../../../Servicios/infoPersonal.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import Swal from "sweetalert2";
+
 @Component({
   selector: 'app-update-info-personal',
   templateUrl: './update-info-personal.component.html',
@@ -20,17 +21,18 @@ export class UpdateInfoPersonalComponent implements OnInit, OnDestroy {
     motivacion: '',
     urlImagen: ''
   }
+
   constructor(public readonly infoPerService: InfoPersonalService,
               private readonly activatedRoute: ActivatedRoute,
-              private readonly router: Router) { }
+              private readonly router: Router) {
+  }
 
   ngOnInit(): void {
-    this.suscripcionId = this.activatedRoute.params.subscribe(params=>{
-      this.infoPerService.getdocumentInfoPersonal(params.id).subscribe(document=>{
-        console.log("documento**************", document)
-        this.informacionPersonalInicial = document
+    this.suscripcionId = this.activatedRoute.params.subscribe(params => {
+      this.infoPerService.getdocumentInfoPersonal(params.id).subscribe(document => {
         this.documentId = params.id
-        this.informacionPersonal = this.informacionPersonalInicial
+        this.informacionPersonal = document
+        this.informacionPersonalInicial = {...this.informacionPersonal} // clonar
       }, error => {
         Swal.fire({
           position: 'top-end',
@@ -48,67 +50,78 @@ export class UpdateInfoPersonalComponent implements OnInit, OnDestroy {
       })
     })
   }
-  ocultarModalInfoPersonal(){
+
+  ocultarModalInfoPersonal() {
 
   }
-  async regresar(){
-    try{
+
+  async regresar() {
+    try {
       await this.router.navigateByUrl('/administracion/informacion-personal')
-    }catch (e){
+    } catch (e) {
       console.log(e)
     }
   }
-  async actualizarDatosmodal(forma: any){
+
+  async actualizarDatosmodal(forma: any) {
     let resp
-    console.log("informacionPersonalInicial ---------", this.informacionPersonalInicial)
-    console.log("informacionPersonal -------------", this.informacionPersonal)
-    // const respSwal = await Swal.fire({
-    //   title: 'Desea actualizar los datos?',
-    //   text: 'Tus datos actualizados podrás modificarlos después',
-    //   icon: 'warning',
-    //   showCancelButton: true,
-    //   confirmButtonColor: '#3085d6',
-    //   cancelButtonColor: '#d33',
-    //   confirmButtonText: 'Si, actualizar los datos'
-    // })
-    // if(respSwal.isConfirmed){
-    //   try {
-    //     resp = await this.infoPerService.actualizarInfoPersonal(this.documentId, this.informacionPersonal)
-    //   }catch (e) {
-    //     Swal.fire({
-    //       position: 'top-end',
-    //       icon: 'error',
-    //       title: 'Hubo un problema',
-    //       text: 'No se pudo actualizar la información',
-    //     })
-    //   }
-    //   if(resp){
-    //     await this.router.navigateByUrl('/administracion/informacion-personal')
-    //     await Swal.fire({
-    //       position: 'top-end',
-    //       icon: 'success',
-    //       title: 'Datos actualizados',
-    //       showConfirmButton: false,
-    //       timer: 1500
-    //     })
-    //   }else{
-    //     await Swal.fire({
-    //       position: 'top-end',
-    //       icon: 'error',
-    //       title: 'Hubo un problema',
-    //       text: 'No se pudo actualizar la información',
-    //     })
-    //   }
-    // }
+    if (JSON.stringify(this.informacionPersonal) === JSON.stringify(this.informacionPersonalInicial)) {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'warning',
+        title: 'No se puede actualizar',
+        text: 'Los datos no han cambiado',
+      })
+    } else {
+      const respSwal = await Swal.fire({
+        title: 'Desea actualizar los datos?',
+        text: 'Tus datos actualizados podrás modificarlos después',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, actualizar los datos'
+      })
+      if (respSwal.isConfirmed) {
+        try {
+          resp = await this.infoPerService.actualizarInfoPersonal(this.documentId, this.informacionPersonal)
+        } catch (e) {
+          Swal.fire({
+            position: 'top-end',
+            icon: 'error',
+            title: 'Hubo un problema',
+            text: 'No se pudo actualizar la información',
+          })
+        }
+        if (resp) {
+          await this.router.navigateByUrl('/administracion/informacion-personal')
+          await Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Datos actualizados',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        } else {
+          await Swal.fire({
+            position: 'top-end',
+            icon: 'error',
+            title: 'Hubo un problema',
+            text: 'No se pudo actualizar la información',
+          })
+        }
+      }
+    }
   }
-  conseguirUrlImg(url: any){
+  conseguirUrlImg(url: any) {
     this.informacionPersonal.urlImagen = url
   }
+
   ngOnDestroy(): void {
-    if(this.suscripcionId){
+    if (this.suscripcionId) {
       this.suscripcionId.unsubscribe()
     }
-    if(this.suscripcionDocument){
+    if (this.suscripcionDocument) {
       this.suscripcionDocument.unsubscribe()
     }
   }
