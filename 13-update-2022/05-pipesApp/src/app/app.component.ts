28f0ce9a9f8a714ebccbc39820ad8bcea638b237
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2023-03-23 23:20:06 -0500

    fix: change to app fieldset

--- 13-update-2022/05-pipesApp/src/app/app.component.ts
+++ 13-update-2022/05-pipesApp/src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   nombre: string = 'Jonathan';
   valor: number = 1000;
   obj = {
-    nombre: 'Jonathan'
+    nombre: 'Jonathan',
+  };
+
+  constructor(private primeNgConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
   }
-
-
 }
--- 13-update-2022/05-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 13-update-2022/05-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
   styleUrls: ['./no-comunes.component.css'],
 })
-export class NoComunesComponent implements OnInit {
-  constructor(private primeNgConfig: PrimeNGConfig) {}
-
-  ngOnInit(): void {
-    this.primeNgConfig.ripple = true;
-  }
+export class NoComunesComponent {
+  constructor() {}
 }
