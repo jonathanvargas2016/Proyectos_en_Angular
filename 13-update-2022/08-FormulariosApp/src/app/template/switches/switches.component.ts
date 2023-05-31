17be462f3703e93d82b3701da635b9544cad12d8
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2023-05-30 23:10:52 -0500

    feat: template forms

--- 13-update-2022/08-FormulariosApp/src/app/template/switches/switches.component.ts
+++ 13-update-2022/08-FormulariosApp/src/app/template/switches/switches.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.css']
+  styleUrls: ['./switches.component.css'],
 })
 export class SwitchesComponent implements OnInit {
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
 
-  constructor() { }
+  terminosCondiciones: boolean = false;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
